581ed902bc5dc6c7685835f9d11a68118cbd57f3
KaRob121 <karlrobin_espinosa@yahoo.com>
2021-02-15 12:44:00 -0700

    Completed 328. Odd Even Linked List

---
+++ Medium/328. Odd Even Linked List/328_odd_even_linked_list.py
@@ -0,0 +1,50 @@
+"""
+Given a singly linked list, group all odd nodes together followed by the even nodes. Please note here we are talking about the node number and not the value in the nodes.
+
+You should try to do it in place. The program should run in O(1) space complexity and O(nodes) time complexity.
+
+Constraints:
+
+- The relative order inside both the even and odd groups should remain as it was in the input.
+- The first node is considered odd, the second node even and so on ...
+- The length of the linked list is between [0, 10^4].
+"""
+
+# THOUGHTS:
+# Make sure to consider edge cases! I didn't think about the edge case where the given list is empty, which caused a Runtime Error.
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def oddEvenList(self, head: ListNode) -> ListNode:
+        """
+        Using a two pointer approach, groups all elements in odd places in the given list 
+        followed by the elements in even places with O(1) space and O(n) time complexity
+        """
+        
+        # Checks if the provided linked list is empty and returns if it is
+        if not head:
+            return head
+    
+        # Heads of odd and even lists
+        odd_head = head
+        even_head = head.next
+        # Iterators
+        odd = odd_head
+        even = even_head
+        
+        # Groups all elements in odd places in the list together; same for elements in even places 
+        while (odd.next != None) and (even.next != None):
+            odd.next = even.next
+            odd = odd.next
+            
+            even.next = odd.next
+            even = even.next
+            
+        # Appends the even list to the end of the odd list
+        odd.next = even_head
+        
+        return odd_head
